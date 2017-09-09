65eb9dd7e379ef213652e836781701436694e4c8
David Belnap <david.belnap6@gmail.com>
2017-09-09 14:50:57 -0600

    landing page stylized

--- dronegram/app/assets/javascripts/application.js
+++ dronegram/app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require rails-ujs
 //= require jquery
 //= require materialize-sprockets
-//= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
