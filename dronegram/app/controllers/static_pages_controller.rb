c704fe6071e5cf3ec80856ebc8d5a7be03794bd4
Adam Kimball <adamchasekimball@gmail.com>
2017-09-09 15:01:06 -0600

    bio controller

--- dronegram/app/controllers/static_pages_controller.rb
+++ dronegram/app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+  # before_action :set_bio, only: [:home2]
+
+
   def home
     if signed_in?
       redirect_to static_pages_home2_path
@@ -6,6 +9,13 @@ class StaticPagesController < ApplicationController
   end
 
   def home2
+    @user = current_user
+    @bio = @user.bio
   end
 
+  # private
+  #
+  #   def set_bio
+  #     @bio = current_user.bios.find(params[:user_id])
+  #   end
 end
--- dronegram/config/routes.rb
+++ dronegram/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-
-  resources :bios
-
+  resources :user do
+    resources :bios
+  end
 end
